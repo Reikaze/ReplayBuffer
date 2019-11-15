93880fa858fdd9eafa3db592c991628008a1c814
RedEyeSnappingTurtle <43766774+RedEyeSnappingTurtle@users.noreply.github.com>
2019-11-15 06:03:31 -0800

    Update crc.v

--- crc.v
+++ crc.v
@@ -16,31 +16,43 @@ input rst;
 input [27:0] tlp_in;
 output [63:0] crc_out;
 
-reg [11:0] seq_num = 0;
-reg count;
-reg [63:0] data;
-reg [63:0] crc_out;
+reg [11:0] seq_num = 0;						// everytime we read the next byte assign seq num
+reg count;							// seq_num 12bit + tlp_in 28 bit + 16 bit 0s = 64 bits
+reg [63:0] data;						// increment sequence number
+reg [63:0] crc_out;						// append sequence number to beginning of tlp and 16 bit 0 to end
+								// lfsr will take the appended data and output 16 bit crc
+wire [15:0] lfsr_o;						
+wire lfsr_fin;
 
-wire [15:0] lfsr_o;
-
-always@(tlp_in)begin								// everytime we read the next byte assign seq num
-	seq_num = seq_num + 1;							// seq_num 12bit + tlp_in 28 bit + 16 bit 0s = 64 bits
-	data <= {seq_num,tlp_in,16'b0};						// increment sequence number
-end										// append sequence number to beginning of tlp and 16 bit 0 to end
-										// lfsr will take the 
-lfsr #(.NBITS(16)) rr1(.clk(clk),.rst(rst),.we(we),.data(data),.q(lfsr_o));	// .data = inputs     .q = output
-
-always@(posedge clk or data)begin						// every time clock is 1 and there is new "data" 
-	count = 0;
-	if(count <= 64)begin							// counter counts up the 64 bits and write enable = 1
-		count <= count + 1;						// once we hit 64 data stops write and count is initialized for the next byte
-		we <= 1b'1;							// of data
+always@(tlp_in)begin								
+	seq_num = seq_num + 1;							
+	data <= {seq_num,tlp_in,16'b0};						
+end										
+										
+lfsr #(.NBITS(16)) rr1(.clk(clk),.rst(rst),.we(we),.data(data),.q(lfsr_o), .d_fin(lfsr_fin));	
+								// .data = inputs     .q = output
+/*
+always@(posedge clk) begin					// this block might be better than the next... need testing
+	if(lfsr_fin == 1)begin
+		we = 0;
+	else
+		we = 1;
+	end
+end
+*/
+	
+always@(posedge clk or data)begin						
+	count = 0;						// every time clock is 1 and there is new "data" 
+	if(count <= 64)begin					// counter counts up the 64 bits and write enable = 1
+		count <= count + 1;				// once we hit 64 data stops write and count is initialized for the next byte
+		we <= 1b'1;					// of data
 	else
 		we <= 1b'0;
 	end
 end
 
-assign crc_out = {seq_num,tlp_in,lfsr_o};
+assign crc_out = {seq_num,tlp_in,lfsr_o};			// the 16 bits from the lfsr appended to tlp
 
 endmodule
+
 
