3c1e5e97683716d4eaea241cc7826b947888becd
RedEyeSnappingTurtle <43766774+RedEyeSnappingTurtle@users.noreply.github.com>
2019-11-25 13:06:19 -0800

    Update lfsrtb.v
    needs work... i cant get q's output

--- lfsrtb.v
+++ lfsrtb.v
@@ -5,8 +5,19 @@ module lfsrtb;
 
 	reg clk;
 	reg we, rst;
-	reg [NUMB-1:0] data;
-	wire [NUMB-1:0] d, q;
+	reg [127:0] data;
+	wire [NUMB-1:0] q;
+	reg [6:0] count;
+
+	reg [127:0] din [0:3];
+	reg [2:0] n = 0;
+
+initial begin
+din[0] = 128'h11112222333344445555666677778888;
+din[1] = 128'h9999aaaabbbbccccddddeeeeffff0000;
+din[2] = 128'h0000aaaabbbbccccddddeeeeffff0000;
+din[3] = 128'h1111aaaabbbb1111ddddeeeeffff1111;
+end
 
 initial
         $vcdpluson;
@@ -16,17 +27,38 @@ lfsr #(.NBITS(NUMB)) rr1
 	.rst(rst),
 	.we(we),
 	.data(data),
-	.q(d));
+	.q(q));
+
+always@(posedge clk, negedge rst)begin
+	if(count < 127) begin
+		#10 we <= 0;
+		count = count + 1;
+	end
+	else begin
+		#10 count = 0;
+		we <= 1;
+		rst <= 0;
+		n <= n + 1; 
+		data <= din[n];
+	end
+end
+
+
+/*
 initial
 begin
         rst = 1'b1;
-	we = 1'b0;
-	data = 16'b0000000000001001;
+//	we = 1'b0;
+	data = 128'h11112222333344445555666677778888;
 	#20 rst = 1'b0;
-	#50 we = 1'b1;
-	#100 we = 1'b0;
-end
+//	#10 we = 1'b1;
+//	#10 we = 1'b0;
+	#300 data = 128'h9999aaaabbbbccccddddeeeeffff0000;
+//	#10 we = 1'b1;
+//	#10 we = 1'b0;
 
+end
+*/
 
 initial
 begin
@@ -36,7 +68,7 @@ end
 
 initial
 begin
-       #1000 $finish;
+       #10000 $finish;
 end
 
 
