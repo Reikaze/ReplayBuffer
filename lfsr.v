b955541fff486ea2aec630704dea941cea461e71
RedEyeSnappingTurtle <43766774+RedEyeSnappingTurtle@users.noreply.github.com>
2019-11-27 11:27:45 -0800

    Update lfsr.v

--- lfsr.v
+++ lfsr.v
@@ -1,32 +1,33 @@
 module lfsr #(parameter NBITS)	// parameter not really needed...
 	     (input clk,
 	      input rst,
-	      input we,
 	      input [127:0] data,
 	      output [NBITS-1:0] q);
 
-reg [NBITS:1] d;
+reg [NBITS:1] d = 1;
 reg xorin;
-reg setq;
-reg count = 0;
+reg [6:0] count;
 
 always@(posedge clk, negedge rst)
 begin
-	if(rst)begin
-		d <= 16'b0;
-	end
-	if(we) begin
+	if(!rst)
+		d <= 'b1;
+	if(count < 127) begin
 		d <= data;
+		count = count + 1;
 	end
+	else
+	begin
 		d <= {d[NBITS-1:1], xorin};
+		count = 0;
+	end
+		
 end
-
 
 always@(*)begin
 	xorin = (d[12] ^ d[3] ^ d[1]); 
 end
 
-assign q = d[NBITS-1:1];
-
+assign q = d[NBITS:1];
 
 endmodule
