e99afd2ab989867b562605f85af15513da43927f
RedEyeSnappingTurtle <43766774+RedEyeSnappingTurtle@users.noreply.github.com>
2019-11-27 11:28:25 -0800

    Update lfsrtb.v

--- lfsrtb.v
+++ lfsrtb.v
@@ -4,20 +4,11 @@ module lfsrtb;
 	parameter NUMB = 16;
 
 	reg clk;
-	reg we, rst;
+	reg rst;
 	reg [127:0] data;
 	wire [NUMB-1:0] q;
-	reg [6:0] count;
+	
 
-	reg [127:0] din [0:3];
-	reg [2:0] n = 0;
-
-initial begin
-din[0] = 128'h11112222333344445555666677778888;
-din[1] = 128'h9999aaaabbbbccccddddeeeeffff0000;
-din[2] = 128'h0000aaaabbbbccccddddeeeeffff0000;
-din[3] = 128'h1111aaaabbbb1111ddddeeeeffff1111;
-end
 
 initial
         $vcdpluson;
@@ -25,51 +16,21 @@ initial
 lfsr #(.NBITS(NUMB)) rr1
 	(.clk(clk),
 	.rst(rst),
-	.we(we),
 	.data(data),
 	.q(q));
 
-always@(posedge clk, negedge rst)begin
-	if(count < 127) begin
-		#10 we <= 0;
-		count = count + 1;
-	end
-	else begin
-		#10 count = 0;
-		we <= 1;
-		rst <= 0;
-		n <= n + 1; 
-		data <= din[n];
-	end
-end
-
-
-/*
 initial
-begin
-        rst = 1'b1;
-//	we = 1'b0;
-	data = 128'h11112222333344445555666677778888;
-	#20 rst = 1'b0;
-//	#10 we = 1'b1;
-//	#10 we = 1'b0;
-	#300 data = 128'h9999aaaabbbbccccddddeeeeffff0000;
-//	#10 we = 1'b1;
-//	#10 we = 1'b0;
+       $monitor($time, " rst = %b, data = %h, q = %h\n",  rst,data,q);
 
-end
-*/
 
 initial
 begin
-       clk = 1'b0;
-       forever #10 clk = ~clk;
+        rst = 1'b0;
+#10	data = 128'h11112222333344445555666677771212;
+ 	rst = 1'b1;
+#10 	data = 128'h9999aaaabbbbccccddddeeeeffff5555;
+#100	data = 128'h22223333444455556666777788881161;
+#100	data = 128'h11113333555577779999bbbbddddfffDf;
+#100;
+
 end
-
-initial
-begin
-       #10000 $finish;
-end
-
-
-endmodule
