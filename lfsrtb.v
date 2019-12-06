15d56d9dad32a66a2c6f54c5d2779837ca65c01c
RedEyeSnappingTurtle <43766774+RedEyeSnappingTurtle@users.noreply.github.com>
2019-12-05 19:41:42 -0800

    Update lfsrtb.v

--- lfsrtb.v
+++ lfsrtb.v
@@ -1,36 +1,51 @@
 `include "lfsr.v"
 module lfsrtb;
 
-	parameter NUMB = 16;
+	//parameter NUMB = 16;
 
 	reg clk;
 	reg rst;
+	reg we;
 	reg [127:0] data;
-	wire [NUMB-1:0] q;
-	
+	wire [15:0] q;
+	wire d_rdy;
 
 
 initial
         $vcdpluson;
 
-lfsr #(.NBITS(NUMB)) rr1
+lfsr #(16) rr1
 	(.clk(clk),
 	.rst(rst),
+	.we(we),
 	.data(data),
-	.q(q));
+	.q(q),
+	.rdy(rdy));
 
 initial
-       $monitor($time, " rst = %b, data = %h, q = %h\n",  rst,data,q);
+       $monitor($time, " we = %b rst = %b finish = %b\n\t\t\t data = %h\n\t\t\t d = %b\n\t\t\t q = %b\n", rst, we, rdy, data, lfsr.d, q);
 
 
 initial
 begin
-        rst = 1'b0;
-#10	data = 128'h11112222333344445555666677771212;
- 	rst = 1'b1;
-#10 	data = 128'h9999aaaabbbbccccddddeeeeffff5555;
-#100	data = 128'h22223333444455556666777788881161;
-#100	data = 128'h11113333555577779999bbbbddddfffDf;
-#100;
+        rst = 1'b0; we = 1;
+#10	rst = 1'b1; we = 0;
+	data = 128'h11112222333344445555666677771111;
+/*
+#10	data = 128'h11112222333344445555666688880000;
+ 	
+#200	data = 128'h1111222233334444ffff666677770000;
+#200	data = 128'h1111eeee333344445555666677770000;
+*/
 
+#3000 $finish;
 end
+
+
+initial
+begin
+       clk = 1'b0;
+       forever #10 clk = ~clk;
+end
+
+endmodule
