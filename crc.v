160ad0751561ef8f5625afc751d0c25a8c4029da
victorchars96 <50897287+victorchars96@users.noreply.github.com>
2019-12-06 13:11:10 -0800

    Update crc.v

--- crc.v
+++ crc.v
@@ -3,59 +3,33 @@
 	This code takes a squence number that is appended to 
 	the TLP at the MSB that is received. Then the CRC that
 	is generated will be appended to the LSB of the TLP.
+
+	Waits for LFSR to finish generating 16bit CRC then 
+	appends CRC to TLP and allows output
+
 */
-`include "../lfsr/lfsr.v"
 
-
-module crc(clk, rst, tlp_in, crc_out);
-input clk;
-input rst;
-
+module crc(rdy, tlp_in, lfsr_in, crc_out);
+input rdy;
 input [95:0] tlp_in;
-output [127:0] crc_out;
+input [15:0] lfsr_in;
+output reg [127:0] crc_out;
 
-reg we;
-reg [11:0] seq_num = 0;	
-reg [15:0] seq_out;						// everytime we read the next byte assign seq num
-reg count;							// seq_num (4bit + 12bit) + tlp_in (96 bit) + 16 bit 0s = 96 bits
+reg [11:0] seq_num = 12'b0;	
+reg [15:0] seq_out;						// everytime we read the next byte assign seq num												// seq_num (4bit + 12bit) + tlp_in (96 bit) + 16 bit 0s = 96 bits
 reg [127:0] data;						// increment sequence number
 reg [127:0] crc_out;						// append sequence number to beginning of tlp and 16 bit 0 to end
-
 								// lfsr will take the appended data and output 16 bit crc
-
 wire [15:0] lfsr_o;						
-wire lfsr_fin;
 
-always@(tlp_in)begin								
-	seq_num <= seq_num + 1;	
-	seq_out <= {4'b0,seq_num};						
-	data <= {seq_out,tlp_in,16'b0};						
+
+always@(*)begin				
+	if(rdy) begin		
+	seq_num = seq_num + 1;	
+	seq_out = {4'b0,seq_num};						
+	data = {seq_out,tlp_in,16'b0};					
+	crc_out = {data[127:16],lfsr_in};
+	end
 end										
 										
-lfsr #(.NBITS(16)) rr1(.clk(clk),.rst(rst),.we(we),.data(data),.q(lfsr_o), .d_fin(lfsr_fin));	
-								// .data = inputs     .q = output
-/*
-always@(data) begin					// this block might be better than the next... need testing
-	if(lfsr_fin == 1)begin					// review lfsr.v for better sense
-		we = 0;
-		crc_out <= {data[127:16],lfsr_o};			// the 16 bits from the lfsr appended to tlp
-	end
-	else 
-		we = 1;
-	
-end
-*/
-always@(data)begin						
-	count = 0;						// every time clock is 1 and there is new "data" 
-
-	if(count < 128)begin					// counter counts up the 64 bits and write enable = 1
-		count <= count + 1;				// once we hit 64 data stops write and count is initialized for the next byte
-		we = 1;						// of data
-		crc_out <= {data[127:16],lfsr_o};		// the 16 bits from the lfsr appended to tlp
-	end	
-	else begin
-		we = 0;
-		
-	end
-end
 endmodule
